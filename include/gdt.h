fe1e49101f76a473bb78d5dbbc4eafe347a3d34a
Gennadii Semenyshyn <gennadii.semenyshyn@sportvision.tech>
2021-03-06 17:37:55 +0100

    Fix

--- include/gdt.h
+++ include/gdt.h
@@ -1,26 +1,35 @@
-#ifndef SGG_OS_GDT_H
-#define SGG_OS_GDT_H
+#ifndef __GDT_H
+#define __GDT_H
 
 #include "types.h"
 
 class GlobalDescriptorTable {
 public:
-    class SegmentSelector {
-    public:
-        SegmentSelector(uint32_t base, uint32_t limit, uint8_t flags);
 
-        uint32_t base();
-
-        uint32_t limit();
-
+    class SegmentDescriptor {
     private:
         uint16_t limit_lo;
         uint16_t base_lo;
         uint8_t base_hi;
         uint8_t type;
-        uint8_t flags_limit_hi;
+        uint8_t limit_hi;
         uint8_t base_vhi;
+
+    public:
+        SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
+
+        uint32_t base();
+
+        uint32_t limit();
     } __attribute__((packed));
+
+private:
+    SegmentDescriptor null_segment_selector;
+    SegmentDescriptor unused_segment_selector;
+    SegmentDescriptor code_segment_selector;
+    SegmentDescriptor data_segment_selector;
+
+public:
 
     GlobalDescriptorTable();
 
@@ -29,11 +38,6 @@ public:
     uint16_t codeSegmentSelector();
 
     uint16_t dataSegmentSelector();
-
-    SegmentSelector null_segment_selector;
-    SegmentSelector unused_segment_selector;
-    SegmentSelector code_segment_selector;
-    SegmentSelector data_segment_selector;
 };
 
-#endif //SGG_OS_GDT_H
+#endif
